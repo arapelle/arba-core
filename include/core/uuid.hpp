2eefe8252b1345e33456989a0bbfa864653d7d9b
Aymeric Pellé <aympelle@gmail.com>
2020-09-02 16:19:26 +0200

    v0.1.0:CORE#1-2: Hash function and UUID class.
    core::murmur_hash_64()
    core::uuid

---
+++ include/core/uuid.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string_view>
+#include <array>
+#include <string>
+#include <cstdint>
+
+namespace core
+{
+/**
+ * @brief The uuid class
+ *
+ * Implementation is taken from stduuid: https://github.com/mariusbancila/stduuid/blob/master/include/uuid.h
+ */
+class uuid
+{
+public:
+    uuid();
+    uuid(const std::string_view& str);
+    bool is_nil() const;
+    std::string to_string() const;
+    inline const std::array<uint8_t, 16>& data() const { return data_; }
+    inline std::array<uint8_t, 16>& data() { return data_; }
+
+    auto operator<=>(const uuid&) const = default;
+    friend std::ostream& operator<<(std::ostream& stream, const uuid& uuid);
+
+private:
+    std::array<uint8_t, 16> data_;
+};
+}
+
+namespace std
+{
+   template <>
+   struct hash<core::uuid>
+   {
+      std::size_t operator()(const core::uuid& uuid) const;
+   };
+}
