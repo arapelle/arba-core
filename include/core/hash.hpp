1b75f890f45d782cf36fd81f38d341770aaf6130
Aymeric Pellé <aympelle@gmail.com>
2020-09-04 10:58:20 +0200

    v0.2.0:CORE#3-4: Add new hash functions (portable 64, and portable 128).

--- include/core/hash.hpp
+++ include/core/hash.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 
 namespace core
@@ -9,10 +10,30 @@ namespace core
  * @param data  Array of bytes
  * @param len  Length of data
  * @param seed
- * @return hash
+ * @return hash 64
  *
  * The implementation is taken from the function MurmurHash64A written in the following file:
  * https://github.com/aappleby/smhasher/blob/master/src/MurmurHash2.cpp
  */
-uint64_t murmur_hash_64(const void* data, int len, uint64_t seed = 6364136223846793005ULL);
+uint64_t murmur_hash_64(const void* data, std::size_t len, uint64_t seed = 6364136223846793005ULL);
+
+/**
+ * @param data  Array of bytes
+ * @param len  Length of data
+ * @param seed
+ * @return hash 64
+ *
+ * The implementation is inspired from the function MurmurHashNeutral2 written in the following file:
+ * https://github.com/aappleby/smhasher/blob/master/src/MurmurHash2.cpp
+ */
+uint64_t neutral_murmur_hash_64(const void* data, std::size_t len, uint64_t seed = 6364136223846793005ULL);
+
+/**
+ * @brief neutral_murmur_hash_array_16
+ * @param data  Array of bytes
+ * @param len  Length of data
+ * @param seed
+ * @return hash 128 as 16 uint8_t in a std::array
+ */
+std::array<uint8_t, 16> neutral_murmur_hash_array_16(const void* data, std::size_t len, uint64_t seed = 6364136223846793005ULL);
 }
--- test/hash_tests.cpp
+++ test/hash_tests.cpp
@@ -27,6 +27,56 @@ TEST(core_tests, test_murmur_hash_64_neq_seed)
     ASSERT_NE(hash, hash2);
 }
 
+TEST(core_tests, test_neutral_murmur_hash_64_eq)
+{
+    std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    uint64_t hash = core::neutral_murmur_hash_64(bytes.data(), bytes.size());
+    uint64_t hash2 = core::neutral_murmur_hash_64(bytes.data(), bytes.size());
+    ASSERT_EQ(hash, hash2);
+}
+
+TEST(core_tests, test_neutral_murmur_hash_64_neq_data)
+{
+    std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    uint64_t hash = core::neutral_murmur_hash_64(bytes.data(), bytes.size());
+    std::array<std::uint8_t, 8> other_bytes{ 103, 26, 64, 25, 55, 224, 146, 246 };
+    uint64_t hash2 = core::murmur_hash_64(other_bytes.data(), other_bytes.size());
+    ASSERT_NE(hash, hash2);
+}
+
+TEST(core_tests, test_neutral_murmur_hash_64_neq_seed)
+{
+    std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    uint64_t hash = core::neutral_murmur_hash_64(bytes.data(), bytes.size());
+    uint64_t hash2 = core::neutral_murmur_hash_64(bytes.data(), bytes.size(), 8467930056364136223ULL);
+    ASSERT_NE(hash, hash2);
+}
+
+TEST(core_tests, test_neutral_murmur_hash_array_16_eq)
+{
+    std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    std::array<std::uint8_t, 16> hash = core::neutral_murmur_hash_array_16(bytes.data(), bytes.size());
+    std::array<std::uint8_t, 16> hash2 = core::neutral_murmur_hash_array_16(bytes.data(), bytes.size());
+    ASSERT_EQ(hash, hash2);
+}
+
+TEST(core_tests, test_neutral_murmur_hash_array_16_neq_data)
+{
+    std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    std::array<std::uint8_t, 16> hash = core::neutral_murmur_hash_array_16(bytes.data(), bytes.size());
+    std::array<std::uint8_t, 8> other_bytes{ 103, 26, 64, 25, 55, 224, 146, 246 };
+    std::array<std::uint8_t, 16> hash2 = core::neutral_murmur_hash_array_16(other_bytes.data(), bytes.size());
+    ASSERT_NE(hash, hash2);
+}
+
+TEST(core_tests, test_neutral_murmur_hash_array_16_neq_seed)
+{
+    std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    std::array<std::uint8_t, 16> hash = core::neutral_murmur_hash_array_16(bytes.data(), bytes.size());
+    std::array<std::uint8_t, 16> hash2 = core::neutral_murmur_hash_array_16(bytes.data(), bytes.size(), 8467930056364136223ULL);
+    ASSERT_NE(hash, hash2);
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
