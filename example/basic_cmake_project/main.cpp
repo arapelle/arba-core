af85a8764db40db9cb7767469d2ae4d0852b243b
Aymeric Pellé <aympelle@gmail.com>
2020-09-06 19:13:13 +0200

    v0.3.0: Update basic CMake project example.

--- example/basic_cmake_project/main.cpp
+++ example/basic_cmake_project/main.cpp
@@ -1,9 +1,12 @@
 #include <core/version.hpp>
+#include <core/uuid.hpp>
 #include <iostream>
 #include <cstdlib>
 
 int main()
 {
-    std::cout << CORE_VERSION << std::endl;
+    std::cout << "core-" << CORE_VERSION << std::endl;
+    core::uuid id("03b0d57c-a6bd-40de-bbb7-d26534801bb0");
+    std::cout << "uuid: " << id << std::endl;
     return EXIT_SUCCESS;
 }
