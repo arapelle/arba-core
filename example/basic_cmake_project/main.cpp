f62a0101216b23e6cf3e041b7b6ca885b9ea1304
Aymeric Pellé <aympelle@gmail.com>
2023-07-08 17:24:30 +0200

    v0.14.0:#16: Tidy examples , tests and CMakeLists.txt files.

--- example/basic_cmake_project/main.cpp
+++ example/basic_cmake_project/main.cpp
@@ -1,11 +1,11 @@
-#include <core/version.hpp>
-#include <core/uuid.hpp>
+#include <arba/core/version.hpp>
+#include <arba/core/uuid.hpp>
 #include <iostream>
 #include <cstdlib>
 
 int main()
 {
-    std::cout << "core-" << CORE_VERSION << std::endl;
+    std::cout << "arba-core-" << ARBA_CORE_VERSION << std::endl;
     core::uuid id("03b0d57c-a6bd-40de-bbb7-d26534801bb0");
     std::cout << "uuid: " << id << std::endl;
     return EXIT_SUCCESS;
