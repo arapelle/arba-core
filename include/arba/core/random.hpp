607f84f5a392a5d4abb9d7d739bd0532628e079e
Aymeric Pellé <aympelle@gmail.com>
2023-07-23 14:41:22 +0200

    v0.14.0:#20: Add random int engine adaptor: uniform_engine which embeds a std::uniform_int_distribution.

--- include/arba/core/random.hpp
+++ include/arba/core/random.hpp
@@ -37,5 +37,104 @@ void reseed();
 
 void reseed(private_::rand_int_engine_type_::result_type value);
 
+
+template <class RNG, class IntType, IntType... IntParams>
+class uniform_engine;
+
+template <class RNG, class IntType, IntType MinValue, IntType MaxValue>
+class uniform_engine<RNG, IntType, MinValue, MaxValue> : public RNG
+{
+public:
+    using result_type = IntType;
+    using distribution_type = std::uniform_int_distribution<result_type>;
+
+    explicit uniform_engine(typename RNG::result_type seed)
+        : RNG(seed)
+    {}
+
+    uniform_engine()
+        : uniform_engine(typename RNG::result_type(std::random_device{}()))
+    {}
+
+    result_type operator()()
+    {
+        return std::uniform_int_distribution<result_type>(min(), max())(static_cast<RNG&>(*this));
+    }
+
+    static constexpr result_type min() { return MinValue; }
+
+    static constexpr result_type max() { return MaxValue; }
+};
+
+template <class RNG, class IntType, IntType MaxValue>
+class uniform_engine<RNG, IntType, MaxValue> : public uniform_engine<RNG, IntType, 0, MaxValue>
+{
+public:
+    using uniform_engine<RNG, IntType, 0, MaxValue>::uniform_engine;
+};
+
+template <class RNG, class IntType>
+class uniform_engine<RNG, IntType> : public RNG
+{
+public:
+    using result_type = IntType;
+    using distribution_type = std::uniform_int_distribution<result_type>;
+
+    explicit uniform_engine(typename RNG::result_type seed, distribution_type dist = distribution_type())
+        : RNG(seed), dist_(std::move(dist))
+    {}
+
+    uniform_engine()
+        : uniform_engine(typename RNG::result_type(std::random_device{}()))
+    {}
+
+    uniform_engine(typename RNG::result_type seed, result_type min, result_type max)
+        : uniform_engine(seed, distribution_type(min, max))
+    {}
+
+    explicit uniform_engine(distribution_type dist)
+        : uniform_engine(std::random_device{}(), dist)
+    {}
+
+    uniform_engine(result_type min, result_type max)
+        : uniform_engine(distribution_type(min, max))
+    {}
+
+    result_type operator()()
+    {
+        return dist_(static_cast<RNG&>(*this));
+    }
+
+    const distribution_type& distribution() const { return dist_; }
+    distribution_type& distribution() { return dist_; }
+
+private:
+    distribution_type dist_;
+};
+
+template <std::uint_fast8_t... IntParams>
+using urng_u8 = uniform_engine<std::mt19937, std::uint_fast8_t, IntParams...>;
+
+template <std::int_fast8_t... IntParams>
+using urng_i8 = uniform_engine<std::mt19937, std::int_fast8_t, IntParams...>;
+
+template <std::uint_fast16_t... IntParams>
+using urng_u16 = uniform_engine<std::mt19937, std::uint_fast16_t, IntParams...>;
+
+template <std::int_fast16_t... IntParams>
+using urng_i16 = uniform_engine<std::mt19937, std::int_fast16_t, IntParams...>;
+
+template <std::uint_fast32_t... IntParams>
+using urng_u32 = uniform_engine<std::mt19937, std::uint_fast32_t, IntParams...>;
+
+template <std::int_fast32_t... IntParams>
+using urng_i32 = uniform_engine<std::mt19937, std::int_fast32_t, IntParams...>;
+
+template <std::uint_fast64_t... IntParams>
+using urng_u64 = uniform_engine<std::mt19937_64, std::uint_fast64_t, IntParams...>;
+
+template <std::int_fast64_t... IntParams>
+using urng_i64 = uniform_engine<std::mt19937_64, std::int_fast64_t, IntParams...>;
+
 }
 }
--- test/random_tests.cpp
+++ test/random_tests.cpp
@@ -27,15 +27,15 @@ TEST(random_tests, test_rand_int_min_max)
     {
         uint16_t index = core::rand_int<uint16_t>(1, counters.size() - 1);
         ASSERT_THAT(index, AllOf(Ge(1), Le(counters.size() - 1)));
-        ++counters[index];
+        ++counters.at(index);
     }
 
     EXPECT_EQ(counters.front(), 0);
     std::ranges::for_each(std::ranges::subrange(counters.begin() + 1, counters.end()),
                           [=](const auto& counter)
-    {
-        EXPECT_GE(counter, 0.9 * factor);
-    });
+                          {
+                              EXPECT_GE(counter, 0.9 * factor);
+                          });
 }
 
 TEST(random_tests, test_rand_int_0_max)
@@ -50,12 +50,106 @@ TEST(random_tests, test_rand_int_0_max)
     {
         uint16_t index = core::rand_int<uint16_t>(counters.size() - 1);
         ASSERT_THAT(index, AllOf(Ge(0), Le(counters.size() - 1)));
-        ++counters[index];
+        ++counters.at(index);
     }
 
     std::ranges::for_each(counters,
                           [=](const auto& counter)
+                          {
+                              EXPECT_GE(counter, 0.9 * factor);
+                          });
+}
+
+TEST(random_tests, test_urng_u8_min_max)
+{
+    core::urng_u8<1, 100> rng(42);
+    ASSERT_EQ(rng.min(), 1);
+    ASSERT_EQ(rng.max(), 100);
+
+    std::array<unsigned, decltype(rng)::max() + 1> counters{ 0 };
+    ASSERT_TRUE(std::ranges::all_of(counters, [](auto counter){ return counter == 0; }));
+
+    constexpr unsigned factor = 1000;
+    for (unsigned times = (counters.size() - 1) * factor; times; --times)
     {
-        EXPECT_GE(counter, 0.9 * factor);
-    });
+        uint8_t index = rng();
+        ++counters.at(index);
+    }
+
+    EXPECT_EQ(counters.front(), 0);
+    std::ranges::for_each(std::ranges::subrange(counters.begin() + 1, counters.end()),
+                          [=](const auto& counter)
+                          {
+                              EXPECT_GE(counter, 0.9 * factor);
+                          });
+}
+
+TEST(random_tests, test_urng_u8_max)
+{
+    core::urng_u8<100> rng(42);
+    ASSERT_EQ(rng.min(), 0);
+    ASSERT_EQ(rng.max(), 100);
+
+    std::array<unsigned, decltype(rng)::max() + 1> counters{ 0 };
+    ASSERT_TRUE(std::ranges::all_of(counters, [](auto counter){ return counter == 0; }));
+
+    constexpr unsigned factor = 1000;
+    for (unsigned times = counters.size() * factor; times; --times)
+    {
+        uint8_t index = rng();
+        ++counters.at(index);
+    }
+
+    std::ranges::for_each(counters,
+                          [=](const auto& counter)
+                          {
+                              EXPECT_GE(counter, 0.9 * factor);
+                          });
+}
+
+TEST(random_tests, test_urng_u8)
+{
+    core::urng_u8<> rng(42, 1, 100);
+    ASSERT_EQ(rng.min(), std::mt19937::min());
+    ASSERT_EQ(rng.max(), std::mt19937::max());
+
+    std::array<unsigned, 101> counters{ 0 };
+    ASSERT_TRUE(std::ranges::all_of(counters, [](auto counter){ return counter == 0; }));
+
+    constexpr unsigned factor = 1000;
+    for (unsigned times = (counters.size() - 1) * factor; times; --times)
+    {
+        uint8_t index = rng();
+        ++counters.at(index);
+    }
+
+    EXPECT_EQ(counters.front(), 0);
+    std::ranges::for_each(std::ranges::subrange(counters.begin() + 1, counters.end()),
+                          [=](const auto& counter)
+                          {
+                              EXPECT_GE(counter, 0.9 * factor);
+                          });
+}
+
+TEST(random_tests, test_urng_i8_min_max)
+{
+    core::urng_i8<-2, 3> rng(42);
+    ASSERT_EQ(rng.min(), -2);
+    ASSERT_EQ(rng.max(), 3);
+
+    std::array<unsigned, 6> counters{ 0 };
+    ASSERT_TRUE(std::ranges::all_of(counters, [](auto counter){ return counter == 0; }));
+
+    constexpr unsigned factor = 200;
+    for (unsigned times = counters.size() * factor; times; --times)
+    {
+        uint8_t index = rng() + 2;
+        ++counters.at(index);
+    }
+
+    std::ranges::for_each(counters,
+                          [=](const auto& counter)
+                          {
+                              EXPECT_GE(counter, 0.9 * factor);
+                          });
 }
