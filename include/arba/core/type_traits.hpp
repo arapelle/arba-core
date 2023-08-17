e4b9992e85d13b0f08014d17209caa448179d530
Aymeric Pellé <aympelle@gmail.com>
2023-08-17 20:13:49 +0200

    v0.15.0:#25: Add concept signedness. make_integer<> uses signedness (signed|unsigned) and thread_policy instead of booleans.

--- include/arba/core/type_traits.hpp
+++ include/arba/core/type_traits.hpp
@@ -3,33 +3,37 @@
 #include <type_traits>
 #include <cstdint>
 #include <atomic>
+#include <arba/core/template/thread_policy.hpp>
 
 inline namespace arba
 {
 namespace core
 {
 
-template <uint8_t size, bool is_unsigned = false, bool is_atomic = false>
+template <typename sign_type>
+concept signedness = std::is_same_v<sign_type, signed> || std::is_same_v<sign_type, unsigned>;
+
+template <unsigned bitsize, signedness = signed, thread_policy = thread_unsafe_t>
 struct make_integer;
 
-template <uint8_t size, bool is_unsigned = false, bool is_atomic = false>
-using make_integer_t = typename make_integer<size, is_unsigned, is_atomic>::type;
+template <unsigned bitsize, signedness sign_type = signed, thread_policy th_policy = thread_unsafe_t>
+using make_integer_t = typename make_integer<bitsize, sign_type, th_policy>::type;
 
 template <> struct make_integer<8> { using type = std::int8_t; };
 template <> struct make_integer<16> { using type = std::int16_t; };
 template <> struct make_integer<32> { using type = std::int32_t; };
 template <> struct make_integer<64> { using type = std::int64_t; };
 
-template <uint8_t size>
-struct make_integer<size, true>
+template <unsigned bitsize>
+struct make_integer<bitsize, unsigned>
 {
-    using type = std::make_unsigned_t<make_integer_t<size>>;
+    using type = std::make_unsigned_t<make_integer_t<bitsize>>;
 };
 
-template <uint8_t size, bool is_unsigned>
-struct make_integer<size, is_unsigned, true>
+template <unsigned bitsize, signedness sign_type>
+struct make_integer<bitsize, sign_type, thread_safe_t>
 {
-    using type = std::atomic<make_integer_t<size, is_unsigned>>;
+    using type = std::atomic<make_integer_t<bitsize, sign_type>>;
 };
 
 }
