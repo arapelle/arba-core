870cfac6cd6f2a4f6e53c28e1f2d40bab306293a
Aymeric Pellé <aympelle@gmail.com>
2023-08-18 02:46:52 +0200

    v0.15.0:#26: Add thread_policy types: thread_safe and thread_unsafe. Add thread_policy concept.
    v0.15.0:#26: Add header in CMakeLists.txt.

---
+++ include/arba/core/template/thread_policy.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <type_traits>
+
+inline namespace arba
+{
+namespace core
+{
+
+struct thread_safe_t { explicit thread_safe_t() = default; };
+constexpr thread_safe_t thread_safe;
+
+struct thread_unsafe_t { explicit thread_unsafe_t() = default; };
+constexpr thread_unsafe_t thread_unsafe;
+
+template <class tag_type>
+concept thread_policy = std::is_same_v<tag_type, thread_safe_t> || std::is_same_v<tag_type, thread_unsafe_t>;
+
+}
+}
