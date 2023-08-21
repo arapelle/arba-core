a2ed6e677c0fce8822347ebeae80a68a45351830
Aymeric Pellé <aympelle@gmail.com>
2023-08-21 02:26:44 +0200

    v0.15.0:#17: Add algorithm function quick_erase().

--- include/arba/core/algorithm.hpp
+++ include/arba/core/algorithm.hpp
@@ -10,6 +10,8 @@ inline namespace arba
 namespace core
 {
 
+// unstable_erase()
+
 struct last_value_safe_t { explicit last_value_safe_t() = default; };
 constexpr last_value_safe_t last_value_safe;
 
@@ -42,6 +44,26 @@ inline void unstable_erase(range_type& range, typename range_type::iterator iter
 {
     *iter = std::move(*--std::ranges::end(range));
     range.pop_back();
+}
+
+// quick_erase()
+
+template <std::ranges::random_access_range range_type, last_value_policy last_value_policy>
+inline void quick_erase(range_type& range, std::ranges::iterator_t<range_type> iter, last_value_policy lv_policy)
+{
+    unstable_erase(range, iter, lv_policy);
+}
+
+template <std::ranges::range range_type, last_value_policy last_value_policy>
+inline void quick_erase(range_type& range, std::ranges::iterator_t<range_type> iter, last_value_policy)
+{
+    range.erase(iter);
+}
+
+template <std::ranges::range range_type>
+inline void quick_erase(range_type& range, typename range_type::iterator iter)
+{
+    quick_erase(range, iter, last_value_unsafe);
 }
 
 }
--- test/algorithm_tests.cpp
+++ test/algorithm_tests.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <cstdlib>
 #include <memory>
+#include <list>
 
 TEST(algorithm_tests, test_unstable_erase_string_unsafe_body_item)
 {
@@ -46,4 +47,32 @@ TEST(algorithm_tests, test_unstable_erase_safe)
     core::unstable_erase(vec, iter, core::last_value_safe);
     std::vector<int> expected_vec = { 0, 1, 10, 3, 4, 5, 6 };
     ASSERT_EQ(vec, expected_vec);
+}
+
+
+TEST(algorithm_tests, test_quick_erase_unsafe)
+{
+    std::vector<int> vec = { 0, 1, 2, 3, 4, 5, 6, 10 };
+    auto iter = --vec.end();
+    core::quick_erase(vec, iter);
+    std::vector<int> expected_vec = { 0, 1, 2, 3, 4, 5, 6 };
+    ASSERT_EQ(vec, expected_vec);
+}
+
+TEST(algorithm_tests, test_quick_erase_safe)
+{
+    std::vector<int> vec = { 0, 1, 2, 3, 4, 5, 6, 10 };
+    auto iter = vec.begin();
+    core::quick_erase(vec, iter, core::last_value_safe);
+    std::vector<int> expected_vec = { 10, 1, 2, 3, 4, 5, 6 };
+    ASSERT_EQ(vec, expected_vec);
+}
+
+TEST(algorithm_tests, test_quick_erase_not_random_range)
+{
+    std::list<int> collection = { 0, 1, 2, 3, 4, 5, 6, 10 };
+    auto iter = collection.begin();
+    core::quick_erase(collection, iter);
+    std::list<int> expected_collection = { 1, 2, 3, 4, 5, 6, 10 };
+    ASSERT_EQ(collection, expected_collection);
 }
