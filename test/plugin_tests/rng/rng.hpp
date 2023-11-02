36f11b80402eb542f819ad6beaaf4c2918680fd0
Aymeric Pellé <aympelle@gmail.com>
2023-11-02 12:16:05 +0100

    v0.21.0:#48: In plugin, add make_unique|shared methods taking Args... .

--- test/plugin_tests/rng/rng.hpp
+++ test/plugin_tests/rng/rng.hpp
@@ -6,6 +6,11 @@
 class Generator : public GeneratorInterface
 {
 public:
+    Generator(int a = 0, int b = 1000);
     virtual ~Generator() = default;
     virtual int generate() const override;
+
+private:
+    int a_;
+    int b_;
 };
