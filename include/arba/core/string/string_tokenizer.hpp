f66a4cc7fa2cebd0317ead539bd2d790d6562bb0
Aymeric Pellé <aympelle@gmail.com>
2025-01-23 11:25:21 +0100

    v0.29.0: Rename template parameters with CamelCase names.

--- include/arba/core/string/string_tokenizer.hpp
+++ include/arba/core/string/string_tokenizer.hpp
@@ -9,39 +9,39 @@ inline namespace arba
 namespace core
 {
 
-template <class ch_type>
+template <class CharType>
 struct char_tokenizer
 {
-    explicit char_tokenizer(ch_type sep_ch = 0) : sep_ch_(sep_ch) {}
+    explicit char_tokenizer(CharType sep_ch = 0) : sep_ch_(sep_ch) {}
 
-    inline bool operator()(const ch_type& ch) const { return ch == sep_ch_; }
+    inline bool operator()(const CharType& ch) const { return ch == sep_ch_; }
     inline bool operator==(const char_tokenizer&) const = default;
     inline bool operator!=(const char_tokenizer&) const = default;
 
 private:
-    ch_type sep_ch_;
+    CharType sep_ch_;
 };
 
-template <class ch_type>
+template <class CharType>
 struct char_set_tokenizer
 {
-    explicit char_set_tokenizer(std::basic_string_view<ch_type> sep_chs = 0) : sep_chs_(sep_chs) {}
+    explicit char_set_tokenizer(std::basic_string_view<CharType> sep_chs = 0) : sep_chs_(sep_chs) {}
 
-    inline bool operator()(const ch_type& ch) const { return sep_chs_.find(ch) != sep_chs_.npos; }
+    inline bool operator()(const CharType& ch) const { return sep_chs_.find(ch) != sep_chs_.npos; }
     inline bool operator==(const char_set_tokenizer&) const = default;
     inline bool operator!=(const char_set_tokenizer&) const = default;
 
 private:
-    std::basic_string<ch_type> sep_chs_;
+    std::basic_string<CharType> sep_chs_;
 };
 
-template <class tokenizer_func, class ch_type>
+template <class TokenizerFunction, class CharType>
 class string_view_token_iterator
 {
 public:
-    using char_type = ch_type;
+    using char_type = CharType;
     using string_view = std::basic_string_view<char_type>;
-    using tokenizer_function = tokenizer_func;
+    using tokenizer_function = TokenizerFunction;
     // input_iterator requirements:
     using value_type = string_view;
     using difference_type = std::ptrdiff_t;
@@ -99,11 +99,11 @@ private:
     string_view_iterator token_end_iter_;
 };
 
-template <class tokenizer_func, class ch_type>
-class string_token_iterator : public string_view_token_iterator<tokenizer_func, ch_type>
+template <class TokenizerFunction, class CharType>
+class string_token_iterator : public string_view_token_iterator<TokenizerFunction, CharType>
 {
 private:
-    using base_ = string_view_token_iterator<tokenizer_func, ch_type>;
+    using base_ = string_view_token_iterator<TokenizerFunction, CharType>;
 
 public:
     using typename base_::char_type;
@@ -136,30 +136,30 @@ public:
 namespace impl
 {
 
-template <class tokenizer_function>
+template <class TokenizerFunction>
 class str_tokenizer_function_owner_
 {
 public:
     str_tokenizer_function_owner_() = default;
-    str_tokenizer_function_owner_(tokenizer_function is_ch_sep) : is_ch_sep_(std::move(is_ch_sep)) {}
+    str_tokenizer_function_owner_(TokenizerFunction is_ch_sep) : is_ch_sep_(std::move(is_ch_sep)) {}
 
 protected:
-    tokenizer_function is_ch_sep_;
+    TokenizerFunction is_ch_sep_;
 };
 
-template <class tokenizer_function, class token_iterator>
-class str_tokenizer_ : public str_tokenizer_function_owner_<tokenizer_function>,
-                       public std::ranges::subrange<token_iterator>
+template <class TokenizerFunction, class TokenIterator>
+class str_tokenizer_ : public str_tokenizer_function_owner_<TokenizerFunction>,
+                       public std::ranges::subrange<TokenIterator>
 {
 public:
-    using char_type = typename token_iterator::char_type;
+    using char_type = typename TokenIterator::char_type;
     using string_view = std::basic_string_view<char_type>;
 
 public:
     str_tokenizer_() = default;
-    str_tokenizer_(string_view str, tokenizer_function is_ch_sep)
-        : str_tokenizer_function_owner_<tokenizer_function>(std::move(is_ch_sep)),
-          std::ranges::subrange<token_iterator>(token_iterator(str, this->is_ch_sep_), token_iterator())
+    str_tokenizer_(string_view str, TokenizerFunction is_ch_sep)
+        : str_tokenizer_function_owner_<TokenizerFunction>(std::move(is_ch_sep)),
+          std::ranges::subrange<TokenIterator>(TokenIterator(str, this->is_ch_sep_), TokenIterator())
     {
     }
 };
@@ -168,166 +168,166 @@ public:
 
 // string_view_tokenizer:
 
-template <class tokenizer_func, class char_type = char>
+template <class TokenizerFunction, class CharType = char>
 class string_view_tokenizer;
 
-template <class tokenizer_func, class char_type>
+template <class TokenizerFunction, class CharType>
 class string_view_tokenizer
-    : public impl::str_tokenizer_<tokenizer_func, string_view_token_iterator<tokenizer_func, char_type>>
+    : public impl::str_tokenizer_<TokenizerFunction, string_view_token_iterator<TokenizerFunction, CharType>>
 {
 private:
-    using base_ = impl::str_tokenizer_<tokenizer_func, string_view_token_iterator<tokenizer_func, char_type>>;
+    using base_ = impl::str_tokenizer_<TokenizerFunction, string_view_token_iterator<TokenizerFunction, CharType>>;
 
 public:
     string_view_tokenizer() = default;
-    string_view_tokenizer(typename base_::string_view str, tokenizer_func is_ch_sep) : base_(str, std::move(is_ch_sep))
+    string_view_tokenizer(typename base_::string_view str, TokenizerFunction is_ch_sep) : base_(str, std::move(is_ch_sep))
     {
     }
 };
 
-template <class char_type>
-class string_view_tokenizer<char_type, char_type>
-    : public impl::str_tokenizer_<char_tokenizer<char>,
-                                  string_view_token_iterator<char_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_view_tokenizer<CharType, CharType>
+    : public impl::str_tokenizer_<char_tokenizer<CharType>,
+                                  string_view_token_iterator<char_tokenizer<CharType>, CharType>>
 {
 private:
     using base_ =
-        impl::str_tokenizer_<char_tokenizer<char>, string_view_token_iterator<char_tokenizer<char_type>, char_type>>;
+        impl::str_tokenizer_<char_tokenizer<CharType>, string_view_token_iterator<char_tokenizer<CharType>, CharType>>;
 
 public:
     string_view_tokenizer() = default;
-    string_view_tokenizer(typename base_::string_view str, char_type ch_sep) : base_(str, char_tokenizer<char>(ch_sep))
+    string_view_tokenizer(typename base_::string_view str, CharType ch_sep) : base_(str, char_tokenizer<CharType>(ch_sep))
     {
     }
 };
 
-template <class char_type>
-class string_view_tokenizer<const char_type*, char_type>
-    : public impl::str_tokenizer_<char_set_tokenizer<char>,
-                                  string_view_token_iterator<char_set_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_view_tokenizer<const CharType*, CharType>
+    : public impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                  string_view_token_iterator<char_set_tokenizer<CharType>, CharType>>
 {
 private:
-    using base_ = impl::str_tokenizer_<char_set_tokenizer<char>,
-                                       string_view_token_iterator<char_set_tokenizer<char_type>, char_type>>;
+    using base_ = impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                       string_view_token_iterator<char_set_tokenizer<CharType>, CharType>>;
 
 public:
     string_view_tokenizer() = default;
-    string_view_tokenizer(typename base_::string_view str, const char_type* ch_seps)
-        : base_(str, char_set_tokenizer<char>(ch_seps))
+    string_view_tokenizer(typename base_::string_view str, const CharType* ch_seps)
+        : base_(str, char_set_tokenizer<CharType>(ch_seps))
     {
     }
 };
 
-template <class char_type>
-class string_view_tokenizer<std::basic_string_view<char_type>, char_type>
-    : public impl::str_tokenizer_<char_set_tokenizer<char>,
-                                  string_view_token_iterator<char_set_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_view_tokenizer<std::basic_string_view<CharType>, CharType>
+    : public impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                  string_view_token_iterator<char_set_tokenizer<CharType>, CharType>>
 {
 private:
-    using base_ = impl::str_tokenizer_<char_set_tokenizer<char>,
-                                       string_view_token_iterator<char_set_tokenizer<char_type>, char_type>>;
+    using base_ = impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                       string_view_token_iterator<char_set_tokenizer<CharType>, CharType>>;
 
 public:
     string_view_tokenizer() = default;
-    string_view_tokenizer(typename base_::string_view str, std::basic_string_view<char_type> ch_seps)
-        : base_(str, char_set_tokenizer<char>(ch_seps))
+    string_view_tokenizer(typename base_::string_view str, std::basic_string_view<CharType> ch_seps)
+        : base_(str, char_set_tokenizer<CharType>(ch_seps))
     {
     }
 };
 
-template <class char_type>
-class string_view_tokenizer<std::basic_string<char_type>, char_type>
-    : public impl::str_tokenizer_<char_set_tokenizer<char>,
-                                  string_view_token_iterator<char_set_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_view_tokenizer<std::basic_string<CharType>, CharType>
+    : public impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                  string_view_token_iterator<char_set_tokenizer<CharType>, CharType>>
 {
 private:
-    using base_ = impl::str_tokenizer_<char_set_tokenizer<char>,
-                                       string_view_token_iterator<char_set_tokenizer<char_type>, char_type>>;
+    using base_ = impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                       string_view_token_iterator<char_set_tokenizer<CharType>, CharType>>;
 
 public:
     string_view_tokenizer() = default;
-    string_view_tokenizer(typename base_::string_view str, std::basic_string_view<char_type> ch_seps)
-        : base_(str, char_set_tokenizer<char>(ch_seps))
+    string_view_tokenizer(typename base_::string_view str, std::basic_string_view<CharType> ch_seps)
+        : base_(str, char_set_tokenizer<CharType>(ch_seps))
     {
     }
 };
 
 // string_tokenizer:
 
-template <class tokenizer_func, class char_type = char>
+template <class TokenizerFunction, class CharType = char>
 class string_tokenizer;
 
-template <class tokenizer_func, class char_type>
-class string_tokenizer : public impl::str_tokenizer_<tokenizer_func, string_token_iterator<tokenizer_func, char_type>>
+template <class TokenizerFunction, class CharType>
+class string_tokenizer : public impl::str_tokenizer_<TokenizerFunction, string_token_iterator<TokenizerFunction, CharType>>
 {
 private:
-    using base_ = impl::str_tokenizer_<tokenizer_func, string_token_iterator<tokenizer_func, char_type>>;
+    using base_ = impl::str_tokenizer_<TokenizerFunction, string_token_iterator<TokenizerFunction, CharType>>;
 
 public:
     string_tokenizer() = default;
-    string_tokenizer(typename base_::string_view str, tokenizer_func is_ch_sep) : base_(str, std::move(is_ch_sep)) {}
+    string_tokenizer(typename base_::string_view str, TokenizerFunction is_ch_sep) : base_(str, std::move(is_ch_sep)) {}
 };
 
-template <class char_type>
-class string_tokenizer<char_type, char_type>
-    : public impl::str_tokenizer_<char_tokenizer<char>, string_token_iterator<char_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_tokenizer<CharType, CharType>
+    : public impl::str_tokenizer_<char_tokenizer<CharType>, string_token_iterator<char_tokenizer<CharType>, CharType>>
 {
 private:
     using base_ =
-        impl::str_tokenizer_<char_tokenizer<char>, string_token_iterator<char_tokenizer<char_type>, char_type>>;
+        impl::str_tokenizer_<char_tokenizer<CharType>, string_token_iterator<char_tokenizer<CharType>, CharType>>;
 
 public:
     string_tokenizer() = default;
-    string_tokenizer(typename base_::string_view str, char_type ch_sep) : base_(str, char_tokenizer<char>(ch_sep)) {}
+    string_tokenizer(typename base_::string_view str, CharType ch_sep) : base_(str, char_tokenizer<CharType>(ch_sep)) {}
 };
 
-template <class char_type>
-class string_tokenizer<const char_type*, char_type>
-    : public impl::str_tokenizer_<char_set_tokenizer<char>,
-                                  string_token_iterator<char_set_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_tokenizer<const CharType*, CharType>
+    : public impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                  string_token_iterator<char_set_tokenizer<CharType>, CharType>>
 {
 private:
     using base_ =
-        impl::str_tokenizer_<char_set_tokenizer<char>, string_token_iterator<char_set_tokenizer<char_type>, char_type>>;
+        impl::str_tokenizer_<char_set_tokenizer<CharType>, string_token_iterator<char_set_tokenizer<CharType>, CharType>>;
 
 public:
     string_tokenizer() = default;
-    string_tokenizer(typename base_::string_view str, const char_type* ch_seps)
-        : base_(str, char_set_tokenizer<char>(ch_seps))
+    string_tokenizer(typename base_::string_view str, const CharType* ch_seps)
+        : base_(str, char_set_tokenizer<CharType>(ch_seps))
     {
     }
 };
 
-template <class char_type>
-class string_tokenizer<std::basic_string_view<char_type>, char_type>
-    : public impl::str_tokenizer_<char_set_tokenizer<char>,
-                                  string_token_iterator<char_set_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_tokenizer<std::basic_string_view<CharType>, CharType>
+    : public impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                  string_token_iterator<char_set_tokenizer<CharType>, CharType>>
 {
 private:
     using base_ =
-        impl::str_tokenizer_<char_set_tokenizer<char>, string_token_iterator<char_set_tokenizer<char_type>, char_type>>;
+        impl::str_tokenizer_<char_set_tokenizer<CharType>, string_token_iterator<char_set_tokenizer<CharType>, CharType>>;
 
 public:
     string_tokenizer() = default;
-    string_tokenizer(typename base_::string_view str, std::basic_string_view<char_type> ch_seps)
-        : base_(str, char_set_tokenizer<char>(ch_seps))
+    string_tokenizer(typename base_::string_view str, std::basic_string_view<CharType> ch_seps)
+        : base_(str, char_set_tokenizer<CharType>(ch_seps))
     {
     }
 };
 
-template <class char_type>
-class string_tokenizer<std::basic_string<char_type>, char_type>
-    : public impl::str_tokenizer_<char_set_tokenizer<char>,
-                                  string_token_iterator<char_set_tokenizer<char_type>, char_type>>
+template <class CharType>
+class string_tokenizer<std::basic_string<CharType>, CharType>
+    : public impl::str_tokenizer_<char_set_tokenizer<CharType>,
+                                  string_token_iterator<char_set_tokenizer<CharType>, CharType>>
 {
 private:
     using base_ =
-        impl::str_tokenizer_<char_set_tokenizer<char>, string_token_iterator<char_set_tokenizer<char_type>, char_type>>;
+        impl::str_tokenizer_<char_set_tokenizer<CharType>, string_token_iterator<char_set_tokenizer<CharType>, CharType>>;
 
 public:
     string_tokenizer() = default;
-    string_tokenizer(typename base_::string_view str, std::basic_string_view<char_type> ch_seps)
-        : base_(str, char_set_tokenizer<char>(ch_seps))
+    string_tokenizer(typename base_::string_view str, std::basic_string_view<CharType> ch_seps)
+        : base_(str, char_set_tokenizer<CharType>(ch_seps))
     {
     }
 };
