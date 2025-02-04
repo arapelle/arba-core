415bcda15765d98ef43997874ff6e52720a65ee7
Aymeric Pellé <aympelle@gmail.com>
2025-02-04 23:51:06 +0100

    WIP rebase ok

--- test/enum/crtp_tests.cpp
+++ test/enum/crtp_tests.cpp
@@ -4,9 +4,9 @@
 #include <gtest/gtest.h>
 
 template <class>
-class no_rebase_definition;
+class no_rebind_and_rebase;
 
-template <class SelfType, template <class> class CrtpTemplate = no_rebase_definition>
+template <class SelfType, template <class> class CrtpTemplate = no_rebind_and_rebase>
     requires std::is_class_v<SelfType>
 class crtp_base;
 
@@ -27,22 +27,49 @@ template <class SelfType, template <class> class CrtpTemplate>
 class crtp_base : public crtp_base<SelfType>
 {
 public:
+    template <class OtherType>
+    using rebind_t = CrtpTemplate<OtherType>;
+
     template <class>
     using rebase_t = CrtpTemplate<SelfType>;
 };
 
-class decorator
+class crtp_deco
 {
 public:
     template <class>
-    using rebase_t = decorator;
+    using rebind_t = crtp_deco;
+
+    template <class>
+    using rebase_t = crtp_deco;
 };
+
+template <class Base, class OtherType>
+using rebind_t = typename Base::template rebind_t<OtherType>;
+
+template <class SelfT, class ThisT>
+using conditional_base_t = std::conditional_t<std::is_void_v<SelfT>, ThisT, SelfT>;
 
 template <class Base, class OtherBaseType>
 using rebase_t = typename Base::template rebase_t<OtherBaseType>;
 
-template <class Base, class ThenT, class ElseT>
-using conditional_rebase_t = std::conditional<std::is_same_v<Base, decorator>, ThenT, ElseT>;
+template <class Base, class RebasedType, class RecRebasedType>
+using conditional_rebase_t = std::conditional_t<std::is_same_v<Base, crtp_deco>, RebasedType, RecRebasedType>;
+
+template <class Base, class Decorator, class OtherSelf>
+struct decorate
+{
+    using type = rebind_t<rebase_t<Decorator, Base>, OtherSelf>;
+};
+
+template <class Base, class OtherSelf>
+struct decorate<Base, crtp_deco, OtherSelf>
+{
+    using type = rebind_t<Base, OtherSelf>;
+};
+
+template <class Base, class Decorator, class OtherSelf>
+using decorate_t = typename decorate<Base, Decorator, OtherSelf>::type;
 
 // ------
 
@@ -63,8 +90,13 @@ template <class Base, int Seed>
 class factor : public Base
 {
 public:
+    template <class OtherSelf>
+    using rebind_t = factor<rebind_t<Base, OtherSelf>, Seed>;
+
     template <class OtherBase>
-    using rebase_t = conditional_rebase_t<Base, factor<OtherBase, Seed>, factor<rebase_t<Base, OtherBase>, Seed>>;
+    using rebase_t = conditional_rebase_t<Base,
+                                          factor<OtherBase, Seed>,
+                                          factor<rebase_t<Base, OtherBase>, Seed>>;
 
     int seed() const
     {
@@ -78,6 +110,14 @@ template <class Base, int Offset>
 class offset : public Base
 {
 public:
+    template <class OtherSelf>
+    using rebind_t = offset<rebind_t<Base, OtherSelf>, Offset>;
+
+    template <class OtherBase>
+    using rebase_t = conditional_rebase_t<Base,
+                                          offset<OtherBase, Offset>,
+                                          offset<rebase_t<Base, OtherBase>, Offset>>;
+
     int offset_value() const
     {
         return Offset;
@@ -102,4 +142,31 @@ TEST(crtp_tests, test_crtp_class)
     myclass var;
     int value = var.generate();
     ASSERT_EQ(value, 11142);
+}
+
+class myoffsetbase : public offset<computer<myoffsetbase>, 10000>
+{
+    using base_ = offset<computer<myoffsetbase>, 10000>;
+
+public:
+};
+
+class myfinalbase : public decorate_t<myoffsetbase, factor<crtp_deco, 6>, myfinalbase>
+{
+    using base_ = decorate_t<myoffsetbase, factor<crtp_deco, 6>, myfinalbase>;
+
+public:
+    int seed() const
+    {
+        return 700 + this->base_::seed();
+    }
+
+    int seed_factor() const { return 8; }
+};
+
+TEST(crtp_tests, test_crtp_rebase)
+{
+    myfinalbase var;
+    int value = var.generate();
+    ASSERT_EQ(value, 11748);
 }
